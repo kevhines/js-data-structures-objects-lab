f34e2d3d7b48cb626de22a784f613e5de375b74d
Kevin <kevhines@gmail.com>
2021-04-15 17:03:07 -0400

    commit tree 8b02b20df95af245343b76165454d72b6aea66a9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(oldDriver, key, value) {
+    // const newDriver = {...oldDriver}
+    // newDriver[key] = value
+    // return newDriver
+    return Object.assign({}, oldDriver, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(oldDriver, key, value){
+    oldDriver[key] = value
+    return oldDriver
+}
+
+function deleteFromDriverByKey(oldDriver, key, value) {
+    const newDriver = Object.assign({}, oldDriver);
+    delete newDriver[key]
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(oldDriver, key, value) {
+    delete oldDriver[key]
+    return oldDriver
+}
